89a6ef589b12d068212c2030546f72a426749719
David Riley <Dril112191@gmail.com>
2023-01-05 20:21:07 -0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
